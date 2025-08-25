5c15b6bb880847a52a5ab950068107398e2a8798
alesaayala-arch <alesa.ayala@ucb.edu.bo>
2025-08-24 23:33:53 -0400

    Adiciono el ejercicio 3 de la practica

---
+++ PRACTICA_01/Ejercicio_01_03.cpp
@@ -0,0 +1,21 @@
+// Materia: Programación I, Paralelo 4
+// Autor: Alesa Pierina Ayala Aliaga
+// Carnet: E-17844032 L.P.
+// Carrera del estudiante: Ingeniería Biomédica 
+// Fecha creación: 24/08/2025
+// Número de ejercicio: 3
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float base, altura;
+
+    cout << "Ingresar la base y altura del triangulo:" << endl;
+    cin >> base >> altura;
+
+    cout << "Area del triangulo: " << ((base * altura) / 2.0) << endl;
+
+    return 0;
+}
