9e7f22da25ef4abbe76926e957df34b9aba9cab2
alesaayala-arch <alesa.ayala@ucb.edu.bo>
2025-08-24 22:35:32 -0400

    He subido el primer ejercicio de la practica

---
+++ PRACTICA_01/Ejercicio_01_01.cpp
@@ -0,0 +1,20 @@
+// Materia: Programación I, Paralelo 4
+// Autor: Alesa Pierina Ayala Aliaga
+// Carnet: E-17844032 L.P.
+// Carrera del estudiante: Ingeniería Biomédica 
+// Fecha creación: 24/08/2025
+// Número de ejercicio: 1
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hola, soy estudiante de programación" << endl;
+    cout << "Mi nombre es Alesa Pierina Ayala Aliaga" << endl;
+    cout << "Mi carrera es Ingeniería Biomédica" << endl;
+    
+    return 0;
+
+}
