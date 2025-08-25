9a314ca0402006c809f9d79489dda74734c638a7
alesaayala-arch <alesa.ayala@ucb.edu.bo>
2025-08-24 23:07:11 -0400

    Adiciono el segundo ejercicio de la practica

---
+++ PRACTICA_01/Ejercicio_01_02.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int variable1, variable2;
+    
+    variable1=0;
+    variable2=0;
+
+    cout <<"Valor inicial"<< endl;
+    cout <<"Variable 1: " <<variable1 << endl;
+    cout <<"Variable 2: " <<variable2 << endl;
+
+    cout <<"Ingresar dos valores nuevos" << endl;
+    cin >> variable1 >> variable2;
+
+    cout <<"Nuevos Valores:"<< endl;
+    cout <<"Variable 1: " <<variable1 << endl;
+    cout <<"Variable 2: " <<variable2 << endl;
+
+    return 0;
+
+}
