a03f546b0d853adec1e62989950e498dd444d9c0
alesaayala-arch <alesa.ayala@ucb.edu.bo>
2025-08-26 01:59:08 -0400

    Adiciono el ejercicio 6

---
+++ PRACTICA_01/Ejercicio_01_06.cpp
@@ -0,0 +1,33 @@
+// Materia: Programación I, Paralelo 4
+// Autor: Alesa Pierina Ayala Aliaga
+// Carnet: E-17844032 L.P.
+// Carrera del estudiante: Ingeniería Biomédica 
+// Fecha creación: 26/08/2025
+// Número de ejercicio: 6
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   cout << "Ingresar un numero:" << endl;
+   int numero;
+   cin >> numero;
+
+   if (numero != 0)
+   {
+    if (numero > 0)
+    {
+        cout << "El numero es positivo" << endl;
+    }
+    else
+    {
+        cout << "El numero es negativo" << endl;
+    }
+   }
+   else
+   {
+    cout << "El numero es cero" << endl;
+   }
+}
