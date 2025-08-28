b59a20d9b0d386bf194a3b37daeffa2cdfd6f7fd
alesaayala-arch <alesa.ayala@ucb.edu.bo>
2025-08-27 20:22:35 -0400

    Adiciono el ejercicio 8 de la practica

---
+++ PRACTICA_01/Ejercicio_01_08.cpp
@@ -0,0 +1,29 @@
+// Materia: Programación I, Paralelo 4
+// Autor: Alesa Pierina Ayala Aliaga
+// Carnet: E-17844032 L.P.
+// Carrera del estudiante: Ingeniería Biomédica 
+// Fecha creación: 26/08/2025
+// Número de ejercicio: 8
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero;
+    cout << "Ingresar un numero: " << endl;
+    cin >> numero;
+    
+    if (numero % 2 == 0)
+    {
+        cout <<"El numero es par" << endl;
+    }
+    else 
+    {
+        cout <<"El numero es impar" << endl;
+    }
+
+    return 0;
+
+}
