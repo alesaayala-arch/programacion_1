1fa36943e7da9841b771d5484572305f1947b8c7
alesaayala-arch <alesa.ayala@ucb.edu.bo>
2025-09-10 03:58:40 -0400

    Adiciono el ejercicio 5

---
+++ PRACTICA_02/Ejercicio_02_05.cpp
@@ -0,0 +1,51 @@
+// Materia: Programación I, Paralelo 4
+// Autor: Alesa Pierina Ayala Aliaga
+// Carnet: E-17844032 L.P.
+// Carrera del estudiante: Ingeniería Biomédica 
+// Fecha creación: 10/09/2025
+// Número de ejercicio: 5
+
+#include <iostream>   
+#include <cstdlib>    
+#include <ctime>      
+
+using namespace std;
+
+int main()
+{
+    int n, sumatorio,MeV, MaV;
+    double promedio;
+    sumatorio=0;
+    MeV=1001;
+    MaV=0;
+    cout << "Ingresar la cantidad de numeros:" << endl;
+    cin >> n;
+
+    srand(time(0));
+
+    for (int i=0; i<n; ++i)
+    {
+        int num = (rand() % 1000) + 1;
+        cout << num <<endl;
+        sumatorio += num;
+
+        if (num < MeV)
+        {
+            MeV= num;
+        }
+        
+        if (num > MaV)
+        {
+            MaV=num;
+        }
+    }
+
+    promedio= (sumatorio/n);
+
+    cout << "Numero con mayor valor:"<< MaV <<endl;
+    cout << "Numero con menor Valor:"<< MeV <<endl;
+    cout << "Suma de todos los numero:"<< sumatorio<< endl;
+    cout << "Promedio de todos los numeros:"<< promedio << endl;
+    return 0;
+     
+}
